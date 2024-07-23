4710a954d31b87a514175436e2089aa51d41fbe4
sawankumar-pal-au9 <manojpal211999@gmail.com>
2024-07-23 23:45:36 +0530

    add the transaction logic and impement the unauthorized component

--- FRONTEND/src/components/CustomTable.jsx
+++ FRONTEND/src/components/CustomTable.jsx
@@ -12,32 +12,36 @@ const CustomTable = ({ data }) => {
 
     return (
         <div className="container-fluid p-2">
-            <table className="table table-striped">
-                <thead className="thead-dark">
-                    <tr>
-                        {Object.keys(data[0]).map((item, index) => (
-                            item !== 'id' && <th key={index}>{item}</th>
-                        ))}
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((book, rowIndex) => (
-                        <tr key={rowIndex}>
-                            {Object.keys(book).map((key, colIndex) => (
-                                key !== 'id' &&
-                                <td key={colIndex}>{book[key]}</td>
+            <div className="table-container">
+                <div className="table-responsive">
+                    <table className="table table-striped">
+                        <thead className="thead-dark">
+                            <tr>
+                                {Object.keys(data[0]).map((item, index) => (
+                                    item !== 'id' && <th key={index}>{item}</th>
+                                ))}
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {data.map((book, rowIndex) => (
+                                <tr key={rowIndex}>
+                                    {Object.keys(book).map((key, colIndex) => (
+                                        key !== 'id' &&
+                                        <td key={colIndex}>{book[key]}</td>
+                                    ))}
+                                    <td>
+                                        <div className="btn-group" role="group">
+                                            <button className='btn btn-success btn-sm mr-2' onClick={() => handleAccept(book.id)}>ACCEPT</button>
+                                            <button className='btn btn-danger btn-sm' onClick={() => handleReject(book.id)}>REJECT</button>
+                                        </div>
+                                    </td>
+                                </tr>
                             ))}
-                            <td>
-                                <div className="btn-group" role="group">
-                                    <button className='btn btn-success btn-sm mr-2' onClick={() => handleAccept(book.id)}>ACCEPT</button>
-                                    <button className='btn btn-danger btn-sm' onClick={() => handleReject(book.id)}>REJECT</button>
-                                </div>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };
